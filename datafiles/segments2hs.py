bf900e847d3545bdd7df2c2014cae4339d492fe4
Python3pkg <raliclo@gmail.com>
2017-05-21 08:18:30 -0700

    Convert to Python3

--- datafiles/segments2hs.py
+++ datafiles/segments2hs.py
@@ -6,4 +6,4 @@ line = infile.readline()
 features = [ft.strip() for ft in line.split("|")[2:-1]]
 null = infile.readline()
 pairs = [(r[0].strip(), ",".join([s.strip()+n for (n,s) in zip(features, r[1:])])) for r in [line.split("|")[1:-1] for line in infile]]
-print("segments = [ {0}\n           ]".format("\n           , ".join(['("{0}","[{1}]")'.format(k, v) for (k,v) in pairs])))
+print(("segments = [ {0}\n           ]".format("\n           , ".join(['("{0}","[{1}]")'.format(k, v) for (k,v) in pairs]))))
